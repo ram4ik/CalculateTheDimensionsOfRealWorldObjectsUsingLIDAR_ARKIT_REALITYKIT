10e40f6be2ff755d342204d779d5c40d87dcb1fe
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-05-09 17:30:55 +0300

    Initial Commit

---
+++ CalculateTheDimensionsOfRealWorldObjectsUsingLIDAR_ARKIT_REALITYKIT/CalculateTheDimensionsOfRealWorldObjectsUsingLIDAR_ARKIT_REALITYKITApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateTheDimensionsOfRealWorldObjectsUsingLIDAR_ARKIT_REALITYKITApp.swift
+//  CalculateTheDimensionsOfRealWorldObjectsUsingLIDAR_ARKIT_REALITYKIT
+//
+//  Created by Ramill Ibragimov on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculateTheDimensionsOfRealWorldObjectsUsingLIDAR_ARKIT_REALITYKITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculateTheDimensionsOfRealWorldObjectsUsingLIDAR_ARKIT_REALITYKIT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalculateTheDimensionsOfRealWorldObjectsUsingLIDAR_ARKIT_REALITYKIT
+//
+//  Created by Ramill Ibragimov on 5/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
